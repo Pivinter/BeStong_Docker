dd096f656154dfc07015f56f4c8c46239858b446
Pivinter <vlad.kulia@gmail.com>
2025-04-21 17:07:13 +0300

    added bc

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,21 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
 }
 
+resource "azurerm_storage_account" "terraform_state" {
+  name                     = "tfstatebestrong324"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = var.account_tier_terraform_state
+  account_replication_type = var.account_replication_type_terraform_state
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.terraform_state.id
+  container_access_type = "private"
+}
+
+#App service
 resource "azurerm_service_plan" "asp" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
